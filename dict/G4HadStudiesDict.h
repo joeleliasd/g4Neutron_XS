a592b28c5b161517934906bf35476d84f7effed1
joeleliasd <69224254+joeleliasd@users.noreply.github.com>
2023-03-08 22:09:33 +0900

    Create G4HadStudiesDict.h

---
+++ dict/G4HadStudiesDict.h
@@ -0,0 +1,20 @@
+#ifndef G4HADSTUDIESDICT_H 
+#define G4HADSTUDIESDICT_H 1
+
+// Include files for G4HadStudies dictionary.
+
+/** @file G4HadStudiesDict.h
+ *  
+ *
+ *  @author Chris
+ *  @date   2014
+ */
+
+// here we need to include all the header files
+// for the classes we want to make dictionaries of
+
+#include "../src/ProdTuple_t.hh"
+#include "../src/TrackInfo_t.hh"
+
+
+#endif // G4HADSTUDIESDICT_H
